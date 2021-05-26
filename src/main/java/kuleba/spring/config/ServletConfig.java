b80014a9da504536c63613bf698f073180347e00
Yurii Kuleba <yuriy.kn@gmail.com>
2021-05-26 22:21:36 +0300

    Cleaned up code

--- src/main/java/kuleba/spring/config/ServletConfig.java
+++ src/main/java/kuleba/spring/config/ServletConfig.java
@@ -2,10 +2,7 @@ package kuleba.spring.config;
 
 import kuleba.spring.application.controller.PeopleController;
 import kuleba.spring.dao.PersonDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -28,7 +25,6 @@ public class ServletConfig implements WebMvcConfigurer {
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-//        templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
         return templateResolver;
